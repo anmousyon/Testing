c4130ab7b6bd6a2d863b07f92a951fa57efc7637
Jared Mosley <anmousyon@gmail.com>
2015-10-08 14:05:29 -0500

    added action listener

--- src/afkjava/afkjava.java
+++ src/afkjava/afkjava.java
@@ -4,23 +4,32 @@ import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
-public class afkjava extends JFrame{
-	public static void main(String[] args){
-		JFrame frame1 = new JFrame();
-		JPanel panel1 = new JPanel();
-		JButton button = new JButton("hi");
-		JTextField textfield = new JTextField();
-		
-		frame1.add(panel1);
-		panel1.add(button);
-		panel1.add(textfield);
-		frame1.setSize(400, 400);
-		frame1.setLocation(2900, 500);
-		textfield.setText("Whats up?");
-		frame1.setLayout(new FlowLayout());
-		button.setActionCommand("clicked");
-			
-		frame1.setVisible(true);
+public class afkjava extends JFrame implements ActionListener{
+
+public void ActionPerformed(ActionEvent e){
+	if(e.getSource() == button){
+		String newText = "Not Much";
 	}
+
+}
+
+public static void main(String[] args){
+								JFrame frame1 = new JFrame();
+								frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+								JPanel panel1 = new JPanel();
+								JButton button = new JButton("hi");
+								JTextField textfield = new JTextField();
+
+								frame1.add(panel1);
+								panel1.add(button);
+								panel1.add(textfield);
+								frame1.setSize(400, 400);
+								frame1.setLocation(2900, 500);
+								textfield.setText("Whats up?");
+								frame1.setLayout(new FlowLayout());
+								button.addActionListener(this);
+								frame1.setVisible(true);
+								textfield.setText(newText);
+}
 
 }
