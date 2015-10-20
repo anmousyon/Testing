1fa04f9e3f031a232768da5815c6788b2c26e84e
Jared Mosley <anmousyon@gmail.com>
2015-10-20 02:05:18 -0500

    now working, no known bugs

--- src/DentalOfficeTester.java
+++ src/DentalOfficeTester.java
@@ -1,31 +1,36 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class DentalOfficeTester {
-	public static void main(String[] args){
-		DentalOfficeWindow mainFrame = new DentalOfficeWindow();
-		mainFrame.setTitle("UTD Dental Services");
-		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		mainFrame.setSize(450, 300);
-		mainFrame.setVisible(true);
-		mainFrame.setResizable(false);
+	public static void main(String s[])
+	{
+		DentalOfficeWindow myFrame = new DentalOfficeWindow();
+		myFrame.setTitle("UTD Dental Services");
+		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		myFrame.setSize(450,300);
+		myFrame.setVisible(true);
+		myFrame.setResizable(false);
+
 	}
 }
 
 class DentalOffice{
-	private ArrayList<Double> costs;
+	private ArrayList<Double> costs = new ArrayList<Double>();
 	public DentalOffice(ArrayList<String> services, ArrayList<String> durations){
 		String[] tempService = {"Cavity", "Root Canal", "Braces", "Bridge", "Teeth Cleaning"};
 		for (int i=0; i<tempService.length; i++){
 			services.add(tempService[i]);
 		}
-		String[]tempDuration = {"one week", "one month", "one yer"};
+		String[]tempDuration = {"one week", "one month", "one year"};
 		for (int i=0; i<tempDuration.length; i++){
-			durations.add(tempDurations[i]);
+			durations.add(tempDuration[i]);
 		}
-		double tempCosts[] = {200.00, 600.00, 2000.00, 500.00, 100.00};
+		double[] tempCosts = {200.00, 600.00, 2000.00, 500.00, 100.00};
 		for (int i=0; i<tempCosts.length; i++){
 			costs.add(tempCosts[i]);
 		}
@@ -39,7 +44,7 @@ class DentalOfficeWindow extends JFrame implements ActionListener, ItemListener{
 	private ArrayList<String> services;
 	private ArrayList<String> durations;
 	private JRadioButton jrbuttons[];
-	private ServicePanel servicePanel;
+	private ServicePanel servicesPanel;
 	private JPanel durationPanel;
 	private JPanel actionPanel;
 	private JButton button1;
@@ -47,7 +52,7 @@ class DentalOfficeWindow extends JFrame implements ActionListener, ItemListener{
 	private JLabel label2;
 	private JLabel label3;
 	private Icon dentalIcon = new ImageIcon("dentalOffice.png");
-	private double total =0 ;
+	private double total =0;
 	private String returnTime;
 	private DentalOffice office;
 	private PatientPanel patientPanel;
@@ -64,13 +69,13 @@ class DentalOfficeWindow extends JFrame implements ActionListener, ItemListener{
 		outFrame.setVisible(true);
 		outFrame.setResizable(false);
 		
-		services = new ServicePanel();
-		durations = new JPanel();
-		office = new DentalOffice();
+		services = new ArrayList<String>();
+		durations = new ArrayList<String>();
+		office = new DentalOffice(services, durations);
 		
 		setLayout(new BorderLayout());
-		servicesPanel = new ServicePanel(servicesPanel);
-		durationPanel = new JPanel(durationPanel);
+		servicesPanel = new ServicePanel(services);
+		durationPanel = new JPanel();
 		durationPanel.setLayout(new FlowLayout());
 
 		actionPanel = new JPanel(new GridLayout(2,1));
@@ -81,19 +86,20 @@ class DentalOfficeWindow extends JFrame implements ActionListener, ItemListener{
 		label3 = new JLabel(dentalIcon);
 		label1 = new JLabel("Return in");
 		durationPanel.add(label1);
-		patientPanel = new PatientPanel(patientPanel);
+
+		patientPanel = new PatientPanel();
 
 		for(int i=0; i<jrbuttons.length; i++){
-			jrbuttons[i] = new JRadioButton(office.get(i));
+			jrbuttons[i] = new JRadioButton(durations.get(i));
 			jrbuttons[i].addItemListener(this);
 			group1.add(jrbuttons[i]);
-			durationPanel.add(group1);
+			durationPanel.add(jrbuttons[i]);
 		}
 
 		add(servicesPanel, BorderLayout.WEST);
 		add(label3, BorderLayout.CENTER);
 		add(durationPanel, BorderLayout.NORTH);
-		label2 = new JLABEL("Total cost: $", SwingConstants.CENTER);
+		label2 = new JLabel("Total cost: $", SwingConstants.CENTER);
 		actionPanel.add(label2);
 		actionPanel.add(button1);
 
@@ -113,12 +119,12 @@ class DentalOfficeWindow extends JFrame implements ActionListener, ItemListener{
 		String message = "";
 		total = 0;
 		boolean checkbox = false;
-		boolean radioButton = false;
+		boolean radiobutton = false;
 
-		for(int k =0; k<servicesPanel.length; k++){
+		for(int k =0; k<services.size(); k++){
 			if (servicesPanel.isSelected(k)){
 				checkbox =true;
-				total += office.getCosts(k);
+				total += office.getCost(k);
 			}
 		}
 
@@ -130,10 +136,9 @@ class DentalOfficeWindow extends JFrame implements ActionListener, ItemListener{
 			}
 		}
 		
-		if(patientPanel.getTest() != null && checkbox && radiobutton) {
+		if(patientPanel.getText() != null && checkbox && radiobutton) {
 			label2.setText(message);
 			outFrame.setText(patientPanel.getText() + ": " + message + "\n");
-
 			patientPanel.clearText();  //clears the textFields
          		servicesPanel.clear();  // clears the checkboxes
          		for (int i=0; i < jrbuttons.length; i++) 
@@ -153,74 +158,74 @@ class PatientPanel extends JPanel {
    private JPanel personPanel;
    
    PatientPanel() {
-      personPanel = new JPanel();
-      personPanel.setLayout(new GridLayout(2,1));
-      fName = new JLabel("First Name: ", SwingConstants.RIGHT);
-      fNameField = new JTextField(10);
-      lName = new JLabel("Last Name: ", SwingConstants.RIGHT);
-      lNameField = new JTextField(10);
+      	personPanel = new JPanel();
+      	personPanel.setLayout(new GridLayout(2,1));
+      	fName = new JLabel("First Name: ", SwingConstants.RIGHT);
+      	fNameField = new JTextField(10);
+      	lName = new JLabel("Last Name: ", SwingConstants.RIGHT);
+      	lNameField = new JTextField(10);
       
-      personPanel.add(fname);
+      	personPanel.add(fName);
 	personPanel.add(fNameField);
-	personPanel.add(lname);
+	personPanel.add(lName);
 	personPanel.add(lNameField);
-      add(personPanel);
+      	add(personPanel);
    }
   
    public String getText() { //returns the text from the textfields when populated else returns null
-      if (fNameField.length() > 0 && lNameField.length() > 0) {
-         return fNameField.getText() + " " + lNameField.getText();
-      }
-      else {
-	return null;
-   }
-}
+      	if (fNameField.getText().length() > 0 && lNameField.getText().length() > 0) {
+         	return fNameField.getText() + " " + lNameField.getText();
+      	}
+      	else {
+		return null;
+   	}
+	}	
    public void clearText() {
-      fNameField.setText("");
-      lNameField.setText("");
+      	fNameField.setText("");
+      	lNameField.setText("");
    }
 }
 
 
 class ServicePanel extends JPanel {
-   private JPanel actionPanel;
-   private JCheckBox [] checkboxes;
-   public ServicePanel(ArrayList<String> services) {
-      checkboxes = new JCheckBox();
-      actionPanel = new JPanel();
-      actionPanel.setLayout(new GridLayout(5,1));
-      for (int i=0; i<checkboxes.length; i++) {
-         checkboxes[i] = new JCheckBox();
-         actionPanel.add(checkboxes[i]);
-      }        
-      add(actionPanel); 
-   }  
-   public boolean isSelected(int index) {
-      return checkboxes[index].isSelected();
-   }
+ 	private JPanel actionPanel;
+   	private JCheckBox [] checkboxes;
+   	
+	public ServicePanel(ArrayList<String> services) {
+      		checkboxes = new JCheckBox[services.size()];
+      		actionPanel = new JPanel();
+      		actionPanel.setLayout(new GridLayout(5,1));
+      		for (int i=0; i<checkboxes.length; i++) {
+         		checkboxes[i] = new JCheckBox(services.get(i));
+         		actionPanel.add(checkboxes[i]);
+      		}        
+      		add(actionPanel); 
+   	}  
+   	public boolean isSelected(int index) {
+      		return checkboxes[index].isSelected();
+   	}
 
-   public void clear() {
-      for (int i=0; i < checkboxes.length; i++) 
-         checkboxes[i].setSelected(false);
-   }
+   	public void clear() {
+      		for (int i=0; i < checkboxes.length; i++) 
+         	checkboxes[i].setSelected(false);
+   	}
 }
 
 class OutputWindow extends JFrame {
-   private JTextArea output;
-   private JScrollPane scrollPane; 
+   	private JTextArea output;
+   	private JScrollPane scrollPane; 
    
-   public OutputWindow() {  
-      output = new JTextArea();
-      output.setBackground(Color.LIGHT_GRAY);
-      Font font = new Font("Arial", Font.BOLD, 12); 
-      output.setFont(font);
-      output.setEditable(false);
-      scrollPane = new JScrollPane(); // ScrollPane with output area
-      add(scrollPane);
-      
-   }
+   	public OutputWindow() {  
+      	output = new JTextArea();
+      	output.setBackground(Color.LIGHT_GRAY);
+      	Font font = new Font("Arial", Font.BOLD, 12); 
+      	output.setFont(font);
+      	output.setEditable(false);
+      	scrollPane = new JScrollPane(output); // ScrollPane with output area
+      	add(scrollPane);  
+   	}
    
-   public void setText(String s) {
-      output.setText(s);
-   }
+   	public void setText(String s) {
+      		output.append(s);
+   	}
 }
