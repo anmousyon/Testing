69c8625272e29c8513deb47f0425bef9a2946400
Jared Mosley <anmousyon@gmail.com>
2015-11-30 00:20:00 -0600

    works and commented]

--- exam2/linkedLists.java
+++ exam2/linkedLists.java
@@ -4,13 +4,15 @@ import java.lang.Math.*;
 public class linkedLists{
 	public static void main(String[] args){
 		linkedList ll = new linkedList();
-		for(double i =0; i<100; i++){
+		int rand = 0;
+		for(double i =40; i>0; i--){
 			if(i%2 == 0){
-				ll.insertPriority(i+100);
+				ll.insertPriority(i);
 			}
 			else{
 				ll.insertPriority(i);
 			}
+			
 		}
 		ll.display();
 	}
@@ -18,10 +20,13 @@ public class linkedLists{
 
 class linkedList{
 	class node{
+		
+		//next and previous pointers, an array and the current size of the array
 		protected double[] data;
 		protected node next;
 		protected node prev;
 		protected int size;
+		
 		public node(){
 			next = null;
 			prev = null;
@@ -63,7 +68,8 @@ class linkedList{
 				return -1;
 		}
 	}
-
+	
+	//start and end node, current and parent node with their indeces, and size of linkedlist
 	protected node start = null;
 	protected node end = null;
 	protected int size = 0;
@@ -71,6 +77,7 @@ class linkedList{
 	protected node parent = null;
 	protected int currentIndex = 0;
 	protected int parentIndex = 0;
+	protected node tempNode;
 
 	public linkedList(){
 		start = null;
@@ -86,99 +93,133 @@ class linkedList{
 		return size;
 	}
 	
-	public int findParent(){
-		System.out.println("current index = " + currentIndex);
-		int parentIndex = 0;
-		if(currentIndex %2 == 0){
+	//checks if even or odd then finds its parent index by dividing the first child (always even) by two
+	public void findParent(){
+		//System.out.println("\ncurrent index: " + currentIndex);
+		parentIndex = -1;
+		if((currentIndex % 2) == 0){
 			parentIndex = (currentIndex / 2);
 		}
-		else if(currentIndex % 2 != 0){
-			parentIndex = ((currentIndex-1) /2);
+		
+		else if((currentIndex % 2) != 0){
+			parentIndex = ((currentIndex - 1) /2);
+		
 		}
-		System.out.println("parent index = " +parentIndex);
-		return parentIndex;
+		//System.out.println("Parent Index: " + parentIndex);
 	}
-
+	
+	//gets parent value from parent's index then switches the values of the child and parent if the parent is greater than the child
 	public int switchPC(double parentPriority, double priority, double temp, node tempNode){
-		parentIndex = findParent();
-		parentPriority = parent.data[parentIndex];
+		
+		findParent();
+
+		if(parentIndex == -1){
+			System.out.println("ERROR");
+			return -1;
+		}
+
+		parentPriority = parent.data[parentIndex];//getting value at the parentindex of the previous array
+		
+		//switches parent and child if necessary
 		if(parentPriority > priority){
+			//System.out.println("switching  parent: " + parentPriority + " child: " + priority);
 			temp = parentPriority;
 			parent.data[parentIndex] = priority;
 			current.data[currentIndex] = temp;
-			return parentIndex;
+			current = parent;
+			return parentIndex;//returns parent's index so that the current index can be changed
 		}
-		return -1;
+		
+		else{
+			//System.out.println ("not switching  parent: " + parentPriority + " child: " + priority);
+			return -1;//returns -1 if it doesnt need to be switched
+		}
 	}
-
+	
+	//looks for parent and switches them if necessary then changes the value of current index to its new index (where parent was)
 	public void insertSorter(double priority, double temp, double parentPriority, int tempIndex, node tempNode){
+		
 		while(true){
+			
+			//checks if current has a parent array, if it does set parent equal to that node
 			if(current.prev != null){
 				parent = current.prev;
 			}
+
 			else{
 				break;
 			}
+			
+			//setting tempIndex equal to the parent's index
 			tempIndex= switchPC(parentPriority, priority, temp, tempNode);
+			
+			//if it needs to be switched, set the current index to the parent index
 			if(tempIndex == -1){
 				break;
 			}
+
 			else{
 				currentIndex = tempIndex;
 			}
 		}
 	}
 
+	//adds the new value to the array and stores its index then increments the stored size of array
 	public void addPriority(double priority){
-		System.out.println("inserted "+ priority + " at " + current.size);
+
 		current.data[current.size] = priority;
 		currentIndex = current.size;
 		current.size++;
 	}
 
+	//inserts the value into the first available slot
 	public void insertPriority(double priority){
+		
+		//declaring variables that are needed to switch parent and child and to keep track of the current position
 		double temp = 0;
 		double parentPriority = 0;
 		int tempIndex = 0;
-		node tempNode = null;
 		int currentIndex = 0;
-		if(start != null){
-			current = start;
-			System.out.println("start not null");
-		}
-		else{
-			System.out.println("start null");
+		
+		//checks if start is null and creates an array (of size one), and adds it if it is.
+		if(start == null){
+			
 			double[] newArray = new double[1];
-			insertArrayAtStart(newArray);
-			current = start;
+			insertArrayAtEnd(newArray);
 		}
+
+		current = start;
 		tempNode = current;
+		
+		//iterates through each array in the linked list
 		for(int i = 0; i<size; i++){
+
+			//checks if the array is full (each array is a sequential power of two), if it isn't full then it inserts the value at the first available slow
 			if(current.size != Math.pow(2, i)){
-				System.out.println("size: "+current.size);
 				addPriority(priority);
-				//System.out.println("inserted the priority, size: "+current.size);
 				insertSorter(priority, temp, parentPriority, tempIndex, tempNode);
 				break;
 			}
+
+			//if it is full but there is another array in the linked list, go to that array
 			else if (current.getNext() != null){
 				current = current.getNext();
-				//System.out.println("went to next array");
 			}
+
+			//if all the arrays in the linked list are full create a new array and add it to the end 
 			else{
-				double[] newArray = new double[(int)Math.pow(2, i+1)];
+				double[] newArray = new double[(int)Math.pow(2, i+1)];//creating array twice as large as previous array
 				insertArrayAtEnd(newArray);
-				System.out.println("created new array of size: "+Math.pow(2, i+1));
-				current = current.getNext();
+				current = current.getNext();//sets current to the next array
 				addPriority(priority);
-				System.out.println("inserted priority into new array "+ current.size);
-				insertSorter(priority, temp, parentPriority, tempIndex, tempNode);
+				insertSorter(priority, temp, parentPriority, tempIndex, tempNode);//will check if the new value needs to be sorted or not and will sort if necessary
 				break;
 			}
 
 		}
 	}
-
+	
+	//inserts the array at the start of the linkedlist (not used)
 	public void insertArrayAtStart(double[] val){
 		node nptr = new node(val, null, null);
 		size++;
@@ -192,7 +233,8 @@ class linkedList{
 			start = nptr;
 		}
 	}
-
+	
+	//appends the array to the end of the linkedlist (used)
 	public void insertArrayAtEnd(double[] val){
 		node nptr = new node(val, null, null);
 		size++;
@@ -209,7 +251,8 @@ class linkedList{
 			nptr.prev = head;
 		}
 	}
-
+	
+	//inserts the array at a specific position (not used)
 	public void insertArrayAtPosition(double[] val, int pos){
 		node nptr = new node(val, null, null);
 		node ptr = start;
@@ -228,6 +271,7 @@ class linkedList{
 		size++;
 	}
 	
+	//delets a specific array in the linkedlist (not used)
 	public void deleteArrayAtPosition(int pos){
 		if(pos == 1){
 			start = start.getNext();
@@ -261,8 +305,8 @@ class linkedList{
 		size--;
 	}
 	
+	//displays all the elements in the arrays of the linkedlist sequentially
 	public void display(){
-		System.out.println("\nList: ");
 		if (size == 0){
 			System.out.println("empty");
 			return;
@@ -272,15 +316,15 @@ class linkedList{
 			return;
 		}
 		node ptr = start;
-		System.out.println(start.getData(0));
+		System.out.println(start.getData(0)+ "\n");
 		ptr = start.getNext();
 		while(ptr != null){
-			System.out.println("getNext() not  null");
 			for(int i = 0; i <ptr.size; i++){
 				System.out.println(ptr.getData(i));
 			}
 			if(ptr.getNext() != null){
 				ptr = ptr.getNext();
+				System.out.println("\n");
 			}
 			else{
 				ptr = null;
